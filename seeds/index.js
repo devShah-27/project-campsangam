aa8dc2c070b0afdb5af90ea899f2dbec9258f798
Dev Shah <shahdev149@gmail.com>
2024-07-02 00:00:44 +0530

    make partials and add images to campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const newCamp = new Campground({
       location: `${cities[random1000].name}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eos dignissimos quaerat ex possimus eaque est quidem consectetur? Eligendi, cumque consequuntur molestias dolorum ad libero eos sapiente quas quos ab!",
+      price,
     });
     await newCamp.save();
   }
